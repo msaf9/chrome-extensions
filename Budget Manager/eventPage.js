7fd9151d5757378afb08f28df5d876d9f192b8fd
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-15 23:10:48 +0530

    badge implementation

--- Budget Manager/eventPage.js
+++ Budget Manager/eventPage.js
@@ -1,38 +1,46 @@
 var contextMenuItem = {
-    "id": "spendMoney",
-    "title": "SpendMoney",
-    "contexts": ["selection"]
+  id: "spendMoney",
+  title: "SpendMoney",
+  contexts: ["selection"],
 };
 chrome.contextMenus.create(contextMenuItem);
 
 function isInt(value) {
-    return !isNaN(value) && 
-           parseInt(Number(value)) == value && 
-           !isNaN(parseInt(value, 10));
-  }
+  return (
+    !isNaN(value) &&
+    parseInt(Number(value)) == value &&
+    !isNaN(parseInt(value, 10))
+  );
+}
 
-  chrome.contextMenus.onClicked.addListener(function(clickData){   
-    if (clickData.menuItemId == "spendMoney" && clickData.selectionText){    
-        if (isInt(clickData.selectionText)){          
-            chrome.storage.sync.get(['total','limit'], function(budget){
-                var newTotal = 0;
-                if (budget.total){
-                    newTotal += parseInt(budget.total);
-                }
-
-                newTotal += parseInt(clickData.selectionText);
-                chrome.storage.sync.set({'total': newTotal}, function(){               
-                if (newTotal >= budget.limit){
-                    var notifOptions = {
-                        type: 'basic',
-                        iconUrl: 'icon32.png',
-                        title: 'Limit reached!',
-                        message: 'Oops! Limit is exceed.'
-                    };
-                    chrome.notifications.create('limitNotif', notifOptions);
-                    }
-                });
-            });
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+  if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+    if (isInt(clickData.selectionText)) {
+      chrome.storage.sync.get(["total", "limit"], function (budget) {
+        var newTotal = 0;
+        if (budget.total) {
+          newTotal += parseInt(budget.total);
         }
+
+        newTotal += parseInt(clickData.selectionText);
+        chrome.storage.sync.set({ total: newTotal }, function () {
+          if (newTotal >= budget.limit) {
+            var notifOptions = {
+              type: "basic",
+              iconUrl: "icon32.png",
+              title: "Limit reached!",
+              message: "Oops! Limit is exceed.",
+            };
+            chrome.notifications.create("limitNotif", notifOptions);
+          }
+        });
+      });
     }
+  }
+});
+
+chrome.storage.onChanged.addListener(function (changes, storageName) {
+  chrome.browserAction.setBadgeText({
+    text: changes.total.newValue.toString(),
+  });
 });
